a8231a44114f688afc39672d8e47646446165251
matsu <naoto.matsumoto@cetys.edu.mx>
2023-05-24 05:58:36 -0700

    Point of return, about to change database logic

--- app/src/main/java/com/example/chat_pf/activities/ChatSelectActivity.java
+++ app/src/main/java/com/example/chat_pf/activities/ChatSelectActivity.java
@@ -1,16 +1,24 @@
 package com.example.chat_pf.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.chat_pf.R;
 import com.example.chat_pf.databinding.ActivityChatBinding;
 import com.example.chat_pf.databinding.ActivityChatSelectBinding;
+import com.example.chat_pf.models.Message;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -41,6 +49,50 @@ public class ChatSelectActivity extends AppCompatActivity {
     }
 
     public class DatabaseOperations{
+        /**
+         * All operations that require access to the firebase realtime database should be generated as
+         * a method in this class.
+         */
         private final DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        private final String TAG = "DBO";
+
+        public void getLastMessageSent(){
+            mDatabase.child("messages").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot ds, @Nullable String previousChildName) {
+                    Message message = new Message(
+                            ds.child("name").getValue(String.class),
+                            ds.child("text").getValue(String.class),
+                            ds.child("date").getValue(Long.class),
+                            ds.child("id").getValue(String.class));
+                    Log.d(TAG, String.valueOf(message));
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    Log.e(TAG, "loadMessageListener:onChildChanged");
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                    Log.e(TAG, "loadMessageListener:onChildRemoved");
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    Log.e(TAG, "loadMessageListener:onChildMoved");
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.e(TAG, "loadMessageListener:onCancelled");
+                }
+            });
+        }
+    }
+
+    public void generateChatView(LinearLayout layout){
+
     }
 }
